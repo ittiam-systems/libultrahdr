5d2acaa1c67b7a2442339faf21a0f5c9e18c7e93
Ram Mohan M <ram.mohan@ittiam.com>
2024-12-10 22:34:37 +0530

    enable support for xmp, iso to be build time configurable
    Android.bp enables xmp support by default, cmakelists.txt enables iso
    support by default. Based on the build system the encoded bitstream
    may vary. This change has no effect on decoder. The decoder by default
    supports both iso and xmp. If both exists iso packet is preferred.
    
    Test: ./ultrahdr_unit_test

--- lib/src/jpegr.cpp
+++ lib/src/jpegr.cpp
@@ -54,8 +54,16 @@ uhdr_error_info_t applyGainMapGLES(uhdr_raw_image_t* sdr_intent, uhdr_raw_image_
 #endif
 
 // Gain map metadata
+#ifdef UHDR_WRITE_XMP
 static const bool kWriteXmpMetadata = true;
+#else
+static const bool kWriteXmpMetadata = false;
+#endif
+#ifdef UHDR_WRITE_ISO
+static const bool kWriteIso21496_1Metadata = true;
+#else
 static const bool kWriteIso21496_1Metadata = false;
+#endif
 
 static const string kXmpNameSpace = "http://ns.adobe.com/xap/1.0/";
 static const string kIsoNameSpace = "urn:iso:std:iso:ts:21496:-1";
